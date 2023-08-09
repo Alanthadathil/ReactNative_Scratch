290d88b8c8d15c528be7cfbfb19e13cc1b58b79d
Alanthadathil <alanthadathil73@gmail.com>
2023-08-09 12:10:18 +0530

    App Flex layout updated

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import Box from "./components/Box";
 
 export default function App() {
@@ -24,16 +24,16 @@ export default function App() {
             >
                 <Text>StyleSheet Api</Text>
             </View> */}
-            <Box style={{ backgroundColor: "#5f9ea0", flex: 1 }}>Box 1</Box>
-            <Box style={{ backgroundColor: "#faebd7", flex: 3 }}>Box 2</Box>
+            <Box style={{ backgroundColor: "#5f9ea0" }}>Box 1</Box>
+            <Box style={{ backgroundColor: "#faebd7" }}>Box 2</Box>
             <Box style={{ backgroundColor: "#00ffff" }}>Box 3</Box>
             <Box style={{ backgroundColor: "#0000ff" }}>Box 4</Box>
             <Box style={{ backgroundColor: "#8a2be2" }}>Box 5</Box>
             <Box style={{ backgroundColor: "#a52a2a" }}>Box 6</Box>
-            {/* <Box style={{ backgroundColor: "#deb887" }}>Box 7</Box>
+            <Box style={{ backgroundColor: "#deb887" }}>Box 7</Box>
             <Box style={{ backgroundColor: "#7fff00" }}>Box 8</Box>
             <Box style={{ backgroundColor: "#d2691e" }}>Box 9</Box>
-            <Box style={{ backgroundColor: "#dc143c" }}>Box 10</Box> */}
+            <Box style={{ backgroundColor: "#dc143c" }}>Box 10</Box>
         </View>
     );
 }
@@ -41,6 +41,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: "row",
         // flex: 1,
         // backgroundColor: "plum",
         // padding: 60,
